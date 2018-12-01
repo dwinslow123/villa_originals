1c829d240fcb38cd2e8f31efca3f29369c1bec5b
Dan Winslow <Dwinslow123@me.com>
2018-12-01 13:27:06 -0600

    added social media logos to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import { Navbar } from 'reactstrap';
+import { Navbar, NavItem, Nav } from 'reactstrap';
+
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+
+dom.watch();
+library.add(faFacebook, faInstagram);
 
 export const Footer = () => {
   return (
     <Navbar color="dark" className="footer">
+      <Nav className="ml-auto">
+      <NavItem>
+      <a className="social-logo" href="https://facebook.com/villa_originals">
+        <FontAwesomeIcon
+          icon={['fab', 'facebook']}
+          size="3x"
+          inverse
+        />
+      </a>
+      </NavItem>
+      <NavItem>
+      <a className="social-logo" href="https://instagram.com/villa_originals">
+        <FontAwesomeIcon
+          icon={['fab', 'instagram']}
+          size="3x"
+          inverse
+        />
+      </a>
+      </NavItem>
+      </Nav>
     </Navbar>
   );
 };
