b8902fbb689b381bb07f787edf73971b303efead
Dan Winslow <Dwinslow123@me.com>
2018-12-01 17:11:43 -0600

    added carousel for portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,93 @@
 import React, { Component } from 'react';
+import { gallery } from './gallery';
+
+import {
+  Container,
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators } from 'reactstrap';
 
 class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+  }
+  onExiting = () => {
+    this.animating = true;
+  }
+  onExited = () => {
+    this.animating = false;
+  }
+
+  next = () => {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === gallery.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous = () => {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? gallery.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex }); 
+  }
+
+  goToIndex = (newIndex) => {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
   render() {
+    const { activeIndex } = this.state;
+
+    const slides = gallery.map((photo) => {
+      return (
+        <CarouselItem
+          className="photo"
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={photo.src}
+        >
+          <img src={photo.src} alt={photo.altText} />
+        </CarouselItem>
+      );
+    });
+
     return (
-      <div className="portfolio-main">
-        <img src="#" alt="nothing for now" />
+      <div className="main-gallery">
+        <style>
+          {
+            `.photo {
+              max-width: 100%;
+              height: 500%;
+              align-self: center;
+              background: black;
+            }`
+          }
+        </style>
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+      >
+        <CarouselIndicators
+          items={gallery}
+          activeIndex={activeIndex}
+          onClickHandler={this.goToIndex}
+          
+        />
+          { slides }
+        <CarouselControl
+          direction="prev"
+          directionText="Previous"
+          onClickHandler={this.previous}
+        />
+        <CarouselControl
+          direction="next"
+          directionText="Next"
+          onClickHandler={this.next}
+        />
+      </Carousel>
       </div>
     );
   }
---
+++ src/components/gallery.js
@@ -0,0 +1,88 @@
+import React from 'react';
+
+export const gallery = [
+  {
+    src: "img/gallery1.jpg",
+    altText: 'Image 1',
+    caption: 'Testing',
+    header: 'Slide 11 header'
+  },
+  {
+    src: "img/gallery2.jpg",
+    altText: 'Image 2',
+    caption: 'Testing',
+    header: 'Slide 11 header'
+  },
+  {
+    src: "img/gallery3.jpg",
+    altText: 'Image 3',
+    caption: 'Testing',
+    header: 'Slide 11 header'
+  },
+  {
+    src: "img/gallery4.jpg",
+    altText: 'Image 4',
+    caption: 'Testing',
+    header: 'Slide 11 header'
+  },
+  {
+    src: "img/gallery5.jpg",
+    altText: 'Image 5',
+    caption: 'Testing',
+    header: 'Slide 11 header'
+  },
+  {
+    src: "img/gallery6.jpg",
+    altText: 'Image 6',
+    caption: 'Testing',
+    header: 'Slide 11 header'
+  },
+  {
+    src: "img/gallery7.jpg",
+    altText: 'Image 7',
+    caption: 'Testing',
+    header: 'Slide 11 header'
+  },
+  {
+    src: "img/gallery8.jpg",
+    altText: 'Image 8',
+    caption: 'Testing',
+    header: 'Slide 11 header'
+  },
+  {
+    src: "img/gallery9.jpg",
+    altText: 'Image 9',
+    caption: 'Testing',
+    header: 'Slide 11 header'
+  },
+  {
+    src: "img/gallery10.jpg",
+    altText: 'Image 10',
+    caption: 'Testing',
+    header: 'Slide 11 header'
+  },
+  {
+    src: "img/gallery11.jpg",
+    altText: 'Image 11',
+    caption: 'Testing',
+    header: 'Slide 11 header'
+  },
+  {
+    src: "img/gallery12.jpg",
+    altText: 'Image 12',
+    caption: 'Testing',
+    header: 'Slide 11 header'
+  },
+  {
+    src: "img/gallery13.jpg",
+    altText: 'Image 13',
+    caption: 'Testing',
+    header: 'Slide 11 header'
+  },
+  {
+    src: "img/gallery14.jpg",
+    altText: 'Image 14',
+    caption: 'Testing',
+    header: 'Slide 11 header'
+  },
+]
