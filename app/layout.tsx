0ecc61437f9a466b82a06eb89ebf1d5eedd465f1
Dan Winslow <dwinslow@nexient.com>
2025-10-23 09:55:14 -0500

    added analytics'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Delius_Swash_Caps } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 import Nav from '@/app/components/Nav';
 import Footer from '@/app/components/Footer';
 import './globals.css';
@@ -20,6 +21,7 @@ export default function RootLayout({
         <Nav />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
